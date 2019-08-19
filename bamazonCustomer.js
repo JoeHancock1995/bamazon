f413f49400e9a056755788ebb54bd7893b72badc
Joe Hancock <joe.hancock95@gmail.com>
2019-08-18 22:07:16 -0500

    npm installs, some psuedocode

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,6 @@
-// get a connection to mysql
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// connect to mysql.
 // this application then fetches the data of the 10 items ive created in mysql
 // display the items
